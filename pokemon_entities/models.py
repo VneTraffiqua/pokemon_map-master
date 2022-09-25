5deae1aa40b7f01120461d1baf3a896984249eee
VneTraffiqua <safe72again@gmail.com>
2022-09-25 12:01:38 -0700

    Initial commit

---
+++ pokemon_entities/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Pokemon(models.Model):
+    title = models.CharField(max_length=20)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    lat = models.FloatField(max_length=20)
+    lon = models.FloatField(max_length=20)
+    appeared_at = models.DateTimeField(blank=True, null=True)
+    disappeared_at = models.DateTimeField(blank=True, null=True)
