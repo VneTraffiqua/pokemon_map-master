9f15558f793bf1b6b6d2692614cd20039d94fc0a
VneTraffiqua <safe72again@gmail.com>
2022-09-27 06:02:04 -0700

    show pokemons on mainpage map

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=20)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(upload_to='media/', blank=True, null=True)
 
     def __str__(self):
         return f'{self.title}'
@@ -11,13 +11,16 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    level = models.IntegerField(max_length=50)
     lat = models.FloatField(max_length=20)
     lon = models.FloatField(max_length=20)
     appeared_at = models.DateTimeField(blank=True, null=True)
     disappeared_at = models.DateTimeField(blank=True, null=True)
-    level = models.IntegerField(max_length=50)
-    health = models.IntegerField(max_length=50)
-    strength = models.IntegerField(max_length=50)
-    defence = models.IntegerField(max_length=50)
-    stamina = models.IntegerField(max_length=50)
+    health = models.IntegerField(max_length=50, blank=True, null=True)
+    strength = models.IntegerField(max_length=50, blank=True, null=True)
+    defence = models.IntegerField(max_length=50, blank=True, null=True)
+    stamina = models.IntegerField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.pokemon.title} {self.level}lvl'
 
