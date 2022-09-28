a7e8d89caf4138a00cb15631db38f817954f3bc8
VneTraffiqua <safe72again@gmail.com>
2022-09-28 08:04:52 -0700

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -11,15 +11,15 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    level = models.IntegerField(max_length=50)
+    level = models.IntegerField()
     lat = models.FloatField(max_length=20)
     lon = models.FloatField(max_length=20)
     appeared_at = models.DateTimeField(blank=True, null=True)
     disappeared_at = models.DateTimeField(blank=True, null=True)
-    health = models.IntegerField(max_length=50, blank=True, null=True)
-    strength = models.IntegerField(max_length=50, blank=True, null=True)
-    defence = models.IntegerField(max_length=50, blank=True, null=True)
-    stamina = models.IntegerField(max_length=50, blank=True, null=True)
+    health = models.IntegerField(blank=True, null=True)
+    strength = models.IntegerField(blank=True, null=True)
+    defence = models.IntegerField(blank=True, null=True)
+    stamina = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.pokemon.title} {self.level}lvl'
