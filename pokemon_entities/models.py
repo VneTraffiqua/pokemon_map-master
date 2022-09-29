70877f67b56441ac37523435fa7e65f2f9eb50de
VneTraffiqua <safe72again@gmail.com>
2022-09-29 04:02:57 -0700

    Add verbose names

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,17 +2,18 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=20)
-    title_en = models.CharField(max_length=20, blank=True, null=True)
-    title_jp = models.CharField(max_length=20, blank=True, null=True)
-    image = models.ImageField(upload_to='media/', blank=True, null=True)
-    description = models.TextField(max_length=400, blank=True, null=True)
+    title = models.CharField(max_length=20, verbose_name='Название на русском')
+    title_en = models.CharField(max_length=20, blank=True, null=True, verbose_name='Название на ангийском')
+    title_jp = models.CharField(max_length=20, blank=True, null=True, verbose_name='Название на японском')
+    image = models.ImageField(upload_to='media/', blank=True, null=True, verbose_name='Изображение')
+    description = models.TextField(max_length=400, blank=True, null=True, verbose_name='Описание')
     previous_evolution = models.ForeignKey(
         'Pokemon',
         on_delete=models.CASCADE,
         related_name='evolutions',
         blank=True,
-        null=True
+        null=True,
+        verbose_name='предыдущая эволюция'
     )
 
     def __str__(self):
@@ -21,17 +22,20 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(
-        Pokemon, on_delete=models.CASCADE, related_name='pokemons'
+        Pokemon,
+        on_delete=models.CASCADE,
+        related_name='pokemons',
+        verbose_name='Покемон'
     )
-    level = models.IntegerField()
-    lat = models.FloatField(max_length=20)
-    lon = models.FloatField(max_length=20)
-    appeared_at = models.DateTimeField(blank=True, null=True)
-    disappeared_at = models.DateTimeField(blank=True, null=True)
-    health = models.IntegerField(blank=True, null=True)
-    strength = models.IntegerField(blank=True, null=True)
-    defence = models.IntegerField(blank=True, null=True)
-    stamina = models.IntegerField(blank=True, null=True)
+    level = models.IntegerField(verbose_name='Уровень')
+    lat = models.FloatField(max_length=20, verbose_name='Широта')
+    lon = models.FloatField(max_length=20, verbose_name='Долгота')
+    appeared_at = models.DateTimeField(blank=True, null=True, verbose_name='Появился')
+    disappeared_at = models.DateTimeField(blank=True, null=True, verbose_name='Исчез')
+    health = models.IntegerField(blank=True, null=True, verbose_name='Здоровье')
+    strength = models.IntegerField(blank=True, null=True, verbose_name='Сила')
+    defence = models.IntegerField(blank=True, null=True, verbose_name='Защита')
+    stamina = models.IntegerField(blank=True, null=True, verbose_name='Выносливость')
 
     def __str__(self):
         return f'{self.pokemon.title} {self.level}lvl'
